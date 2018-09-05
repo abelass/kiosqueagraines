05f859c74b04158fd429a9ff19a222b4418ed212
abelass <rainer@websimple.be>
2018-09-05 16:07:46 +0200

    import initial

---
+++ lang/paquet-kiosqueagraines_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// K
+	'kiosqueagraines_description' => '',
+	'kiosqueagraines_nom' => 'Kiosque à graines',
+	'kiosqueagraines_slogan' => 'installation des plugin nécessaires',
+);
